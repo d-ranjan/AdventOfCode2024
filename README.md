c6fcfe2cd6cbfd9fc5ccbc2c929d95af7caf8913
Dipu Ranjan <dipuranjanec08@gmail.com>
2024-12-01 23:37:22 +0530

    initial commit with default day00

---
+++ README.md
@@ -0,0 +1,2 @@
+# AdventOfCode2024
+My C++ solution for Advent of Code 2024
---
+++ day00/input.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string_view>
+
+namespace input
+{
+const std::string_view test =
+    R"(test)";
+
+const std::string_view data =
+    R"(data)";
+} // namespace input
